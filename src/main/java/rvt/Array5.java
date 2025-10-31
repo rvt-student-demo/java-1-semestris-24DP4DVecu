149ba7431edfa206ccad4063a632ae2f30065183
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-10-31 10:14:04 +0200

    array5

---
+++ src/main/java/rvt/Array5.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array5 {
+    public static void main(String[] args) {
+        int[] skaitli = {6, 8, 10, 2, 6};
+        for (int i = 0; i < skaitli.length; i++) {
+            System.out.println(skaitli[i]);
+        }
+    }
+}
