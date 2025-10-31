5ea1966868b415353ad860752ed6e1c9cbed61ec
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-10-31 10:15:00 +0200

    dividing2

---
+++ src/main/java/rvt/Dividing2.java
@@ -0,0 +1,25 @@
+package Dividing;
+
+import java.util.Scanner;
+
+public class Dividing2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //program code
+        System.out.println("Let's try if we can travel to the method world: ");
+        greet();
+
+        System.out.println("Looks like we can, let's try again: ");
+        greet();
+        greet();
+        greet();
+
+    }
+
+    // own methods
+
+    public static void greet() {
+        System.out.println("Greetings from the method world!");
+    }
+}
