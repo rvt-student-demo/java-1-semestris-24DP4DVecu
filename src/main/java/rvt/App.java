76ea9444e5eb2ec96090788e5cd60aa329cf546f
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-10-31 10:12:23 +0200

    App

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,14 @@
-package rvt;
-
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        int x = 20;
+        int y = 30;
+        //type - casting
+        // datu tipu konvertēšana
+
+        System.out.println(
+            // x / Double.valueOf(y)
+            // x / (double) y
+             1.0 * x / y
+        );
     }
 }
