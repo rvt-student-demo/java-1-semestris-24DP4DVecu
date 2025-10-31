8688b672743accd5c46eb6ce3134af0169aa3c9e
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-10-31 10:15:37 +0200

    dividing4

---
+++ src/main/java/rvt/Dividing4.java
@@ -0,0 +1,18 @@
+package Dividing;
+
+public class Dividing4 {
+    public static void main(String[] args) {
+        sum(3, 5);
+
+        int number1 = 2;
+        int number2 = 4;
+
+        sum(number1, number2);
+
+    }
+
+    public static void sum(int first, int second) {
+        System.out.println("The sum of numbers " + first + " and " + second + " is " + (first + second));
+        
+    }
+}
