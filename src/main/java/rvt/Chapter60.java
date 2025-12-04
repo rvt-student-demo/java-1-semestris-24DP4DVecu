85e3eb8535347094882a4f6c3c928ca3b6648f57
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-12-04 10:34:12 +0200

    books

---
+++ src/main/java/rvt/Chapter60.java
@@ -0,0 +1,20 @@
+package rvt;
+
+
+public class Chapter60 {
+
+    public static void main(String[] args) {
+        
+        public static void ex1() {
+        }
+        public static void ex2() {}
+        public static void ex3() {}
+        public static void ex4() {}
+    
+
+    int[] skaitli = {6,8,10,2,6};
+    for (int i = 0; i < skaitli.length; i++) {
+        System.out.println(skaitli[i]);
+        }
+    }
+}
---
+++ src/main/java/rvt/detalas.java
@@ -0,0 +1,48 @@
+package rvt;
+import java.util.Scanner;
+
+public class detalas {
+    
+    private static final int BOLT_PRICE = 5;      
+    private static final int NUT_PRICE = 3;      
+    private static final int WASHER_PRICE = 1;    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Skruvju skaits: ");
+        int skruves = sc.nextInt();
+
+        System.out.print("Uzgrieznu skaits: ");
+        int uzgriezni = sc.nextInt();
+
+        System.out.print("Paplaksnu skaits: ");
+        int paplaksnes = sc.nextInt();
+
+        
+        boolean irKluda = false;
+
+        if (uzgriezni < skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz uzgrieznu");
+            irKluda = true;
+        }
+
+        if (paplaksnes < 2 * skruves) {
+            System.out.println("Parbaudi pasutijumu: par maz paplaksnu");
+            irKluda = true;
+        }
+
+        if (!irKluda) {
+            System.out.println("Pasutijums ir kartiba.");
+        }
+
+        
+        int kopejaCena = skruves * BOLT_PRICE
+                       + uzgriezni * NUT_PRICE
+                       + paplaksnes * WASHER_PRICE;
+
+        System.out.println("Kopeja cena: " + kopejaCena);
+
+        sc.close();
+    }
+}
