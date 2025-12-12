dc41303a6af1c06ed51876fbd9c496a7f9f39593
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-12-12 09:36:53 +0200

    book

--- src/main/java/rvt/Delikatese.java
+++ src/main/java/rvt/Delikatese.java
@@ -3,33 +3,38 @@ package rvt;
 import java.util.Scanner;
 
 public class Delikatese {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
 
-        System.out.print("Welcome to Sam and Ella!\nWhat would you like to order?: ");
+        System.out.println("Ievadi preci: ");
         String prece = scanner.nextLine();
 
-        System.out.print("Enter the price: ");
-        double cena = scanner.nextDouble();
+        System.out.println("Ievadi cenu: ");
+        Double cena = scanner.nextDouble();
 
-        System.out.print("Premium delivery?(0 = no; 1 = yes): ");
-        int izvele = scanner.nextInt();
+        System.out.println("Vai Jums ir nepieciešama ekspress piegāde? (0==nē, 1==jā): ");
+        Double ekspress = scanner.nextDouble();
 
-        System.out.println("\nCheque\n");
+        Double piegades_cena = 0.0;
 
-        System.out.println(prece + " = " + cena);
-
-        double total;
-
-        if (izvele == 1) {
-            System.out.println("Premium Delivery: 5.00");
-             total = cena + 5.00;
+        if (cena < 10) {
+            piegades_cena = 2.00;
         } else {
-            System.out.println("Peasant Delivery: 2.50");
-             total = cena + 2.50;
+            piegades_cena = 0.00;
         }
 
-        System.out.println("Total: " + total);
+        if (ekspress == 1) {
+            piegades_cena = piegades_cena + 3.00;
+        }
 
+        Double kopaa = cena + piegades_cena;
+
+        System.out.println("Rēķins:");
+        
+        System.out.println("   Prece: " + prece);
+        System.out.println("   Cena: " + cena);
+        System.out.println("   Piegāde: " + piegades_cena);
+        System.out.println("   Kopā: " + kopaa);
     }
 }
