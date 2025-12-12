05f531d27cca4810be585d2c2a29177bc5e339c1
Dāvis Vecumnieks <a242882dv@rvt.lv>
2025-12-12 09:58:35 +0200

    average of three numbers

--- src/main/java/rvt/javamoc15.java
+++ src/main/java/rvt/javamoc15.java
@@ -6,7 +6,6 @@ public class javamoc15 {
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.print("Input first number: ");
 
         int num1 = in.nextInt();
